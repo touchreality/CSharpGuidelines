6b7adaedb187fcf458e32f610f45aebdc1c2e6b7
demezchristophe <christophe.demez@touchreality.be>
2019-02-13 16:29:12 +0100

    Add the code overview section and .cs file

--- Overview.cs
+++ Overview.cs
@@ -1,49 +1,3 @@
-Coding Guidelines for C#
-================
-
-Theses guidelines provides coding standards, but also good coding practices that we have adopted at TouchReality.
-
-[Basic Principles (AV0000)](_pages/0000_BasicPrinciples.md)
-
-[Class Design (AV1000)](_pages/1000_ClassDesignGuidelines.md)
-
-[Member Design (AV1100)](_pages/1100_MemberDesignGuidelines.md)
-
-[Miscellaneous (AV1200)](_pages/1200_MiscellaneousDesignGuidelines.md)
-
-[Maintainability Guidelines (AV1500)](_pages/1500_MaintainabilityGuidelines.md)
-
-[Naming Conventions (AV1700)](_pages/1700_NamingGuidelines.md)
-
-[Layout Guidelines (AV2400)](_pages/2400_LayoutGuidelines.md)
-
-## To define...
-
-Performance Guidelines (AV1800)
-
-.NET Framework Usage (AV2200)
-
-Comments and XML Documentation (AV2300)
-
-## Notes
-At TouchReality, we are currently in an adoption phase. It mean that the current rules are not strict but are welcomed.
-We ask you to read them, practice them, discuss them together.
-
-Nothing is writen in the stone, it is a working document that is intended to evolve.
-
-To help you in this decision, I’ve assigned a level of importance to each guideline:
-
-![](/assets/images/1.png) Guidelines that you should never skip and should be applicable to all situations
-
-![](/assets/images/2.png) Strongly recommended guidelines
-
-![](/assets/images/3.png) May not be applicable in all situations
-
-Code Overview
-
-The current section gives you a quick overview of our coding convention, of course it doesn't represents all the rules but gives you a quick entry point.
-
-```csharp
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -219,4 +173,3 @@ namespace TouchReality.BusinessObjects
     }
 
 }
-```
